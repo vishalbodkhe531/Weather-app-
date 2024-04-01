17fea12e7826023a034d59aa6679385b14a8e4e9
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2024-04-01 13:33:15 +0530

    remove comment

--- script.js
+++ script.js
@@ -9,15 +9,11 @@ const width = document.querySelector("#width");
 const humidity = document.querySelector("#humidity");
 const AboutCityIcon = document.querySelector("#about-city-icon");
 const CurrentLocation = document.querySelector("#current-location");
-// console.log(input.value);
-// const API_key = "47c87ece2cf18db7119b858d154af4c";
-// document.addEventListener("DOMContentLoaded",convertJson);
 form.addEventListener("submit",city);
 
 CurrentLocation.addEventListener("click",()=>{
     location.reload(true);
 });
-// CurrentLocation.addEventListener("click",OurLocation)
 
 const convertJson = async()=>{
     input_value =  input.value.trim();
@@ -31,14 +27,8 @@ async function city(e){
 if (input.value.match(/^[A-Za-z]+$/)) {
 
     e.preventDefault();
-    // input_value =  input.value.trim();
-    // const URL = await fetch(`https://api.weatherapi.com/v1/current.json?key=5b27a6ef3547402582e62007222306&q=${input.value}`);
-    // const URLOBJ = await URL.json();
-    // console.log(URLOBJ);
-    // console.log(URLOBJ.location["name"])
     console.log(await convertJson());
     const responce  = await convertJson();
-    // CityInnerName.innerHTML = (URLOBJ.location["name"]);
     CityInnerName.innerHTML = (responce.location["name"]);
     localtime.innerHTML = (responce.location["localtime"]);
     Temperature.innerHTML = (`Temperature : ${responce.current["temp_c"]} Degree`);
@@ -50,8 +40,5 @@ if (input.value.match(/^[A-Za-z]+$/)) {
     }else{
         alert("enter a valid syntax");
     }
-
 }
-// // const API = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
-// console.log("hello");
 
